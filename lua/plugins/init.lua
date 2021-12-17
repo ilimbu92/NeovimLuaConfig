fd3042c9e3fa336a468dbffe04a05049a9d1a309
ilimbu92 <ilimbu92@outlook.com>
2021-12-17 05:56:24 +0000

    whichkey

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -31,5 +31,16 @@ return require('packer').startup(function()
 
   use { 'windwp/nvim-autopairs'}
 
+  use {
+    "folke/which-key.nvim",
+    config = function()
+      require("which-key").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  }
+
 end)
 
