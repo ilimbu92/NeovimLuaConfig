66501f8dd0e1a467981d5e0ea30a1de073d71210
ilimbu92 <ilimbu92@outlook.com>
2021-12-17 04:29:32 +0000

    Lualine and Bufferline

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 1. Syntax Highlighting With Treesitter
 2. Settings in Neovim 
+3. Lualine and Bufferline
 
 
--- init.lua
+++ init.lua
@@ -2,3 +2,5 @@ require('plugins')
 require('treesitter-config')
 vim.cmd[[colorscheme nord]]
 require('options')
+require('lualine-config')
+require('bufferline-config')
